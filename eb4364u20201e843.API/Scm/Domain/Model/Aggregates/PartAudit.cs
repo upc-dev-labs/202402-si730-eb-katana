e549ce2204df6431210fe278cb5313af47e044ec
Salim Ramirez <salimramirezm@gmail.com>
2025-07-13 17:25:03 -0500

    feat(scm): add PartAudit partial class for auditing

---
+++ eb4364u20201e843.API/Scm/Domain/Model/Aggregates/PartAudit.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace eb4364u20201e843.API.Scm.Domain.Model.Aggregates;
+
+public partial class Part : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
