6950798e34dc3a134194942d97e20d7924804386
Salim Ramirez <salimramirezm@gmail.com>
2025-07-13 17:23:42 -0500

    feat(scm): add PartType Enumeration

---
+++ eb4364u20201e843.API/Scm/Domain/Model/ValueObjects/EPartType.cs
@@ -0,0 +1,17 @@
+namespace eb4364u20201e843.API.Scm.Domain.Model.ValueObjects;
+
+/// <summary>
+///     Enumeration representing the PartType supported by Katana.
+///     Defines the operation mode for parts in the manufacturing process.
+/// </summary>
+/// <remarks>
+///     Author: Author
+/// </remarks>
+public enum EPartType
+{
+    BuildToPrint = 0,
+    BuildToSpecification = 1,
+    MadeToStock = 2,
+    MadeToOrder = 3,
+    MadeToAssemble = 4
+}
