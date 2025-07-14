a72ca263305c6724ee7ed33a57d1ff13027906f0
Salim Ramirez <salimramirezm@gmail.com>
2025-07-13 21:00:58 -0500

    fix(scrm): change word "Quantity" for "Capacity" in CreatePartCommand.cs

--- eb4364u20201e843.API/SCM/Domain/Model/Commands/CreatePartCommand.cs
+++ eb4364u20201e843.API/SCM/Domain/Model/Commands/CreatePartCommand.cs
@@ -3,5 +3,5 @@ namespace eb4364u20201e843.API.SCM.Domain.Model.Commands;
 public record CreatePartCommand(
     string Name,
     string PartType,
-    int MaxProductionQuantity
+    int MaxProductionCapacity
 );
