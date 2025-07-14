cdf5fbe491196259b630896690fde00d3721c828
Salim Ramirez <salimramirezm@gmail.com>
2025-07-13 19:24:04 -0500

    docs(mrp): improve documentation of BillOfMaterialsItem.cs aggregate

--- eb4364u20201e843.API/MRP/Domain/Model/Aggregates/BillOfMaterialsItem.cs
+++ eb4364u20201e843.API/MRP/Domain/Model/Aggregates/BillOfMaterialsItem.cs
@@ -3,6 +3,12 @@ using eb4364u20201e843.API.MRP.Domain.Model.ValueObjects;
 
 namespace eb4364u20201e843.API.MRP.Domain.Model.Aggregates;
 
+/// <summary>
+///     Aggregate root representing a Bill Of Materials Item in the MRP context.
+/// </summary>
+/// /// <remarks>
+///     Author: Author
+/// </remarks>
 public partial class BillOfMaterialsItem
 {
     public int Id { get; }
@@ -19,11 +25,18 @@ public partial class BillOfMaterialsItem
 
     public DateTime RequiredAt { get; private set; }
 
+    /// <summary>
+    ///     Parameterless constructor required by EF Core.
+    /// </summary>
     public BillOfMaterialsItem()
     {
         ItemPartNumber = new ItemPartNumber();
     }
 
+    /// <summary>
+    ///     Creates a new BillOfMaterialsItem using the provided command data.
+    /// </summary>
+    /// <param name="command">The command containing the item data.</param>
     public BillOfMaterialsItem(CreateBillOfMaterialsItemCommand command)
     {
         BillOfMaterialsId = command.BillOfMaterialsId;
